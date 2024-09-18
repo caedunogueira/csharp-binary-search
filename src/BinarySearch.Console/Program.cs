8784166c6b345e53b337a92f02142ec48939e741
Carlos Eduardo Nogueira <carlosedu_nogueira@yahoo.com.br>
2024-09-18 09:14:52 -0300

    Implemented simple console version to provide the items for sorted list, the item to use Binary Search and show the result

--- src/BinarySearch.Console/Program.cs
+++ src/BinarySearch.Console/Program.cs
@@ -1,2 +1,28 @@
-﻿Console.WriteLine("Binary Search example!");
+﻿using BinarySearch.Console.Models;
+
+Console.WriteLine("*** Binary Search example ***");
+Console.WriteLine("*****************************");
 Console.WriteLine();
+
+Console.Write("Provide sorted items separated by comma: ");
+var sortedItems = Console.ReadLine()!
+    .Split(",")
+    .Select(x => x.Trim())
+    .Select(int.Parse)
+    .ToArray();
+
+Console.Write("Provide item you would like to find in that list: ");
+var item = int.Parse(Console.ReadLine()!);
+
+var mySortedList = new MySortedList();
+mySortedList.Add(sortedItems);
+var itemIndex = mySortedList.IndexOf(item);
+
+Console.WriteLine();
+
+if (itemIndex != null)
+    Console.WriteLine($"The element was found at index {itemIndex}.");
+else
+    Console.WriteLine("The element was not found in the list.");
+
+Console.ReadKey();
